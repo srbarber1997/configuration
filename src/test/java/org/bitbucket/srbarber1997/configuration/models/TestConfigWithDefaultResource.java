20a85fb5142c5028b4a78ac8a07705df215e886c
Simon <srbarber1997@gmail.com>
2020-04-15 15:51:05 +0100

    fixed mistake with not removing old packages

--- src/test/java/org/bitbucket/srbarber1997/configuration/models/TestConfigWithDefaultResource.java
+++
@@ -1,8 +0,0 @@
-package org.bitbucket.srbarber1997.configuration.models;
-
-import org.bitbucket.srbarber1997.configuration.ConfigurationModel;
-
-@ConfigurationModel(defaultResource = "configResources/modelDefaultResource.config")
-public class TestConfigWithDefaultResource {
-    public int num = 6;
-}
